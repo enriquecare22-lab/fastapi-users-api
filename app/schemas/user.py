8ab46c318c643fe9fe90037b6b8996321edefb90
Enrique <enriquecare22@gmail.com>
2026-04-25 12:26:40 -0600

    refactor: move user schemas into schemas module

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,11 +4,19 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     name: str
     email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
 
 #Schema para respuesta (salida)
-class UserResponse(UserCreate):
+class UserResponse(BaseModel):
     id: int
+    name: str
+    email: str
 
     class Config:
         orm_model = True #permite usar objetos de SQLALchemy
         
+
