515b4fcd06b25f1b24d86f485cb46f652720677a
Enrique <enriquecare22@gmail.com>
2026-04-25 13:03:13 -0600

    feat: add user router with register and login, listar endpoints

---
+++ app/routers/user.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app.schemas.user import UserCreate, UserLogin, UserResponse
+from app.services.user_service import create_user, login_user
+
+# ROUTER (NDPOINTS)
+#---------------
+
+router = APIRouter
+
+#Dependecy para obtener conexion a DB
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+#REGISTER
+
+@router.post("/register")
+def register(user: UserCreate, db:Session =Depends(get_db)):
+    """
+    Endpoit para registrar usuario
+    """
+    create_user(db, user.name,user.email, user.password)
+    return {"message" : "Usuario creado correctamente" }
+
+#LOGIN
+
+@router.post("/login")
+def login(user:UserLogin, db: Session= Depends(get_db)):
+    """
+    Endpoint de autenticacion
+    Devueleve JWT si las credenciales son correctas
+    """
+    token = login_user(db, user.email, user.password)
+
+    if not token:
+        raise HTTPException(status_code=400, detail="CRedenciales inválidas")
+    
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+        }
+
+
+#Listar Usuarios
+
+@router.get("/users", response_model=list[UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    """
+    Devueleve lista de usuarios (sin password)
+    """
+    return db.query(User).all()
+
