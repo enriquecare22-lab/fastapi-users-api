2881f95ad763c91bdc56cd9aba1564c7cb592cd0
Enrique <enriquecare22@gmail.com>
2026-04-25 16:03:27 -0600

    feat: implement user service logic for authentication and CRUD operations

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,10 +1,21 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.core.auth import hash_password, verify_password, create_acces_token
+from app.core.auth import hash_password, verify_password, create_access_token
 
-def create_user(db:Session, name:str, email:str, password:str):
+def create_user(db: Session, name: str, email: str, password: str):
+    #verificar si ya existe el usuario
+    existing =db.query(User).filter(User.email == email).first()
+    if existing:
+        return None
+    
     hashed = hash_password(password)
-    user = User(name= name, email = email, password = hashed)
+    
+    user = User(
+        name = name,
+        email = email,
+        password = hashed
+    )
+    
     db.add(user)
     db.commit()
     db.refresh(user)
@@ -23,5 +34,9 @@ def login_user(db: Session, email: str, password: str):
     token = create_access_token({"sub": user.email})
     return token
     
+
+def get_all_users(db:Session):
+    return db.query(User).all()
+
 
     
